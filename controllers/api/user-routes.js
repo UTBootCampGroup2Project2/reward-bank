66af10b41e4b2aed599b884f6ea4d02677d28c04
hayleyvuylsteke <72625026+hayleyvuylsteke@users.noreply.github.com>
2021-03-18 13:33:14 -0400

    login and logout routes added

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -134,11 +134,46 @@ router.delete('/:id', withAuth, (req, res) => {
   })
 });
 
+//LOGIN Route
 router.post('/login', (req, res) => {
+    User.findOne({
+        where: {
+            username: req.body.username
+        }
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(404).json({ message: 'Sorry! No user was found with that username!'})
+            return
+        }
+
+        const validPassword = dbUserData.checkPassword(req.body.password)
+
+        if (!validPassword) {
+            res.status(400).json({ message: 'Sorry! The password entered was incorrect.'})
+            return
+        }
+
+        req.session.save(() => {
+            req.session.user_id = dbUserData.id
+            req.session.username = dbUserData.username
+            req.session.loggedIn = true
+
+            res.json({ user: dbUserData, message: 'You are now logged in!'})
+        })
+    })
   
 });
 
 router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end()
+        })
+    }
+    else {
+        res.status(404).end()
+    }
   
 });
 
