168b74fb310bb4953469cbb670397ddda4f76dd5
cerafinn <74612014+cerafinn@users.noreply.github.com>
2021-03-17 21:50:06 -0400

    added files for reward and task routes

---
+++ controllers/api/reward-routes.js
@@ -0,0 +1,28 @@
+const router = require('express').Router();
+
+// GET /api/rewards
+router.get('/', (req, res) => {
+
+});
+
+// GET /api/reward/1
+router.get('/:id', (req, res) => {
+  
+});
+
+// POST /api/rewards
+router.post('/', (req, res) => {
+  
+});
+
+// PUT /api/reward/1
+router.put('/:id', (req, res) => {
+  
+});
+
+// DELETE /api/reward/1
+router.delete('/:id', (req, res) => {
+  
+});
+
+module.exports = router;
---
+++ controllers/api/task-routes.js
@@ -0,0 +1,28 @@
+const router = require('express').Router();
+
+// GET /api/tasks
+router.get('/', (req, res) => {
+
+});
+
+// GET /api/task/1
+router.get('/:id', (req, res) => {
+  
+});
+
+// POST /api/tasks
+router.post('/', (req, res) => {
+  
+});
+
+// PUT /api/task/1
+router.put('/:id', (req, res) => {
+  
+});
+
+// DELETE /api/task/1
+router.delete('/:id', (req, res) => {
+  
+});
+
+module.exports = router;
