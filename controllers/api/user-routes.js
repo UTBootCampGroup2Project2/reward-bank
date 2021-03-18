98b792fdb4cd1d93d7a0c16789ae17778ac5f6e4
hayleyvuylsteke <72625026+hayleyvuylsteke@users.noreply.github.com>
2021-03-18 12:51:51 -0400

    user get all and get one added

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,16 +1,46 @@
 const router = require('express').Router();
-const { User, Post} = require("../../models");
+const { User} = require("../../models");
 const withAuth = require('../../utils/auth');
 
 // GET /api/users
 router.get('/', (req, res) => {
+    User.findAll()
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+        console.log(err)
+        res.status(500).json(err)
+    })
 
 });
 
 // GET /api/users/1
 router.get('/:id', (req, res) => {
+    User.findOne({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbUserData => {
+        if (!dbUserData) {
+            res.status(404).json({ message: 'Sorry! No user was found with this id.'})
+            return
+        }
+        res.json(dbUserData)
+    })
+    .catch(err => {
+        console.log(err)
+        res.status(500).json(err)
+    })
   
 });
+
+
+
+//GET all children of a parent
+router.get('/child/:id', (req, res) => {
+  
+});
+
 
 // POST /api/users
 router.post('/', (req, res) => {
