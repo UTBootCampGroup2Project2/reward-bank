2599d6b05dd8f05a8217973f9537b7cee4216959
hayleyvuylsteke <72625026+hayleyvuylsteke@users.noreply.github.com>
2021-03-19 20:56:37 -0400

    updates to task-history and reward-history added

--- controllers/api/reward-history-routes.js
+++ controllers/api/reward-history-routes.js
@@ -1,29 +1,29 @@
 const router = require('express').Router();
-const { User, Task_History, Task } = require("../../models");
+const { User, Reward_History, Reward } = require("../../models");
 const withAuth = require('../../utils/auth');
 
 //Get all task history for a child 
 router.get('/:childID', (req, res) => {
-    Task_History.findAll({
-        attributes: ['id', 'task_id', 'completed_by_user_id', 'status'],
+    Reward_History.findAll({
+        attributes: ['id', 'reward_id', 'purchased_by_user_id'],
         order: [['created_at', 'DESC']],
         where: {
             completed_by_user_id: req.params.childID
         },
         include: [
             {
-                model: Task,
-                attributes: ['name', 'value']
+                model: Reward,
+                attributes: ['name', 'cost']
             }
         ]
 
     })
-    .then(dbTaskHistoryData => {
-        if (!dbTaskHistoryData) {
+    .then(dbRewardHistoryData => {
+        if (!dbRewardHistoryData) {
             res.status(404).json({ message: 'Sorry! No task history was found for this user.'})
             return
         }
-        res.json(dbTaskHistoryData)
+        res.json(dbRewardHistoryData)
     })
     .catch(err => {
         console.log(err)
@@ -35,12 +35,11 @@ router.get('/:childID', (req, res) => {
 
 //add new task history
 router.post('/:childId', (req, res) => {
-    Task_History.create({
-        task_id: req.body.task_id,
-        completed_by_user_id: req.body.completed_by_user_id,
-        status: 'pending',
+    Reward_History.create({
+        reward_id: req.body.reward_id,
+        completed_by_user_id: req.params.childId
     })
-    .then(dbTaskHistoryData => res.json(dbTaskHistoryData))
+    .then(dbRewardHistoryData => res.json(dbRewardHistoryData))
     .catch(err => {
         console.log(err)
         res.status(500).json(err)
@@ -48,14 +47,13 @@ router.post('/:childId', (req, res) => {
 })
 
 //change task history status to complete
-router.put('/:taskHistoryId', (req, res) => {
-    Task_History.update({
-        id: req.params.taskHistoryId,
-        task_id: req.body.task_id,
-        completed_by_user_id: req.params.childId,
-        status: req.body.status,
+router.put('/:rewardHistoryId', (req, res) => {
+    Reward_History.update({
+        id: req.params.rewardHistoryId,
+        reward_id: req.body.reward_id,
+        completed_by_user_id: req.body.completed_by_user_id,
     })
-    .then(dbTaskHistoryData => res.json(dbTaskHistoryData))
+    .then(dbRewardHistoryData => res.json(dbRewardHistoryData))
     .catch(err => {
         console.log(err)
         res.status(500).json(err)
@@ -64,17 +62,17 @@ router.put('/:taskHistoryId', (req, res) => {
 
 //delete task history
 router.delete('/:id', withAuth, (req, res) => {
-    Task_History.destroy({
+    Reward_History.destroy({
         where: {
             id: req.params.id
         }
     })
-    .then(dbTaskHistoryData => {
-        if (!dbTaskHistoryData) {
+    .then(dbRewardHistoryData => {
+        if (!dbRewardHistoryData) {
             res.status(404).json({ message: 'Sorry! No task history was found with this id.'})
             return
         }
-        res.json(dbTaskHistoryData)
+        res.json(dbRewardHistoryData)
     })
     .catch(err => {
         console.log(err)
