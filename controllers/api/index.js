e72a21cbc6f6d4655ba718a1e52dc7d68c49c194
hayleyvuylsteke <72625026+hayleyvuylsteke@users.noreply.github.com>
2021-03-19 20:58:54 -0400

    updates to api/index to add in history routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,12 @@
 const router = require('express').Router();
 
 const userRoutes = require('./user-routes');
-
+const taskHistoryRoutes = require('./task-history-routes');
+const rewardHistoryRoutes = require('./reward-history-routes');
 
 router.use('/users', userRoutes);
+router.use('/task-history', taskHistoryRoutes);
+router.use('/reward-history', rewardHistoryRoutes);
 
 
 
