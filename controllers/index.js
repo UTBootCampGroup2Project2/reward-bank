c8ae10758fa6593404a4daaf0596c1b6813c4a10
celaeon <celaeon@gmail.com>
2021-03-21 22:36:56 -0400

    added dashboard route

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes.js');
-
+const dashboardRoutes = require('./dashboard-routes.js');
 
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
+router.use('/', dashboardRoutes);
 
 router.use((req, res) => {
   res.status(404).end();
