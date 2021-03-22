a1991a3f6937326b6f318db5beebf0840913404c
celaeon <celaeon@gmail.com>
2021-03-22 11:50:10 -0400

    redo dashboard page to work for all device sizes

--- controllers/index.js
+++ controllers/index.js
@@ -5,7 +5,7 @@ const dashboardRoutes = require('./dashboard-routes.js');
 
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
-router.use('/', dashboardRoutes);
+router.use('/dashboard', dashboardRoutes);
 
 router.use((req, res) => {
   res.status(404).end();
--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -1,37 +1,35 @@
 async function newFormHandler(event) {
   event.preventDefault();
 
-  const title = document.querySelector('input[name="post-title"]').value;
-  const post_content = document.querySelector('textarea[name="post-content"]').value;
+  const username = document.querySelector('input').value;
 
-  if(title){
-    const response = await fetch(`/api/posts`, {
-      method: 'POST',
-      body: JSON.stringify({
-        title,
-        post_content
-      }),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
+  const id = window.location.toString().split('/')[
+    window.location.toString().split('/').length - 1
+  ];
   
-    if (response.ok) {
-      document.location.replace('/dashboard');
-    } else {
-      alert(response.statusText);
+
+  const response = await fetch(`/api/users/${id}`, {
+    method: 'GET',
+    body: JSON.stringify({
+      username,
+    }),
+    headers: {
+      'Content-Type': 'application/json'
     }
-  }
-  else{
-    alert('Please enter a title!');
+  });
+
+  if (response.ok) {
+    document.location.replace('/dashboard');
+  } else {
+    alert(response.statusText);
   }
 }
 
 async function createFormHandler(event) {
   event.preventDefault();
 
-  document.querySelector('.add-new-post-section').style.display = "none";
-  document.querySelector('.new-post-section').style.display = "unset";
+  document.querySelector('.approve-list').style.display = "";
+ // document.querySelector('.new-post-section').style.display = "unset";
 }
-document.querySelector('.create-post-btn').addEventListener('click', createFormHandler);
-document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
+// document.querySelector('.create-post-btn').addEventListener('click', createFormHandler);
+// document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
