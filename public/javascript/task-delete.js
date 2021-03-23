e0add06eea25159b854fbc180bbecd3fcfa59d90
BrianCKWang <brian.ck.wang@gmail.com>
2021-03-23 14:05:29 -0400

    implemented delete reward

--- public/javascript/task-delete.js
+++ public/javascript/task-delete.js
@@ -1,10 +1,8 @@
 async function deleteTaskHandler(event) {
   event.preventDefault();
-
-  // console.log(event.toElement.parentNode);
   
   const task_id = event.toElement.getAttribute('task_id');
-  event.toElement.parentNode.remove();
+  
   fetch(`/api/tasks/${task_id}`, {
     method: 'DELETE'
   })
