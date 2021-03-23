0bbc6d9ef865ca68d392f9db4fc5e7d45a266d78
BrianCKWang <brian.ck.wang@gmail.com>
2021-03-22 21:13:50 -0400

    connected name and children balances on dashboard

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,15 +1,46 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { User, } = require('../models');
+const { User, Task_History } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', withAuth, (req, res) => {
-    console.log(req.session);
-    console.log('======================');
+    // console.log('======================');
+    // console.log(req.session);
+    let user;
+    let children;
+
+   User.findOne({
+       where: {id: req.session.user_id},
+       attributes: [
+        'name'
+       ]
+   })
+   .then(dbUserData => {
+        user = dbUserData.toJSON();
+        console.log(user);
+   })
+   .then(() => {
+        return User.findAll({
+            where: {admin_id: req.session.user_id},
+            attributes: [
+                'name',
+                'balance'
+            ]
+        })
+   })
+   .then(dbUserData => {
+        children = dbUserData.map(user => user.get({ plain: true }));
+        console.log(children);
+    })
+   .then(() => {
+        res.render('dashboard', { user, children, loggedIn: req.session.loggedIn});
+   })
+   .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+
     
-    res.render('dashboard', {
-        loggedIn: req.session.loggedIn
-        });
 });
 
 router.get('/edit/:id', withAuth, (req, res) => {
