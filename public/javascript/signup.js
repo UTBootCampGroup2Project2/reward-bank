408fb1a09e84d9347aaaa3ade03f3f2c0efbe4e2
BrianCKWang <brian.ck.wang@gmail.com>
2021-03-26 11:54:46 -0400

    auto login after signing up

--- public/javascript/signup.js
+++ public/javascript/signup.js
@@ -4,26 +4,48 @@ async function signupFormHandler(event) {
   const name = document.querySelector('#name-signup').value.trim();
   const username = document.querySelector('#username-signup').value.trim();
   const password = document.querySelector('#password-signup').value.trim();
-  
-  if (name && username && password) {
-    const response = await fetch('/api/users/child', {
+  const role = document.querySelector('#signup-role').value.trim();
+
+  if (name && username && password && role) {
+    fetch('/api/users', {
       method: 'post',
       body: JSON.stringify({
         name,
         username,
-        password
+        password,
+        role
       }),
       headers: { 'Content-Type': 'application/json' }
+    })
+    .then(response => {
+      // check the response status
+      if (response.ok) {
+        // console.log('Signup success');
+        // document.location.replace('/dashboard');
+      } else {
+        console.log(response);
+        alert(response.statusText);
+      }
+    })
+    .then(() => {
+      // login after signing up
+      return fetch('/api/users/login', {
+        method: 'post',
+        body: JSON.stringify({
+          username,
+          password
+        }),
+        headers: { 'Content-Type': 'application/json' }
+      });
+    })
+    .then(response => {
+      if (response.ok) {
+        document.location.replace('/dashboard/');
+      } else {
+        console.log(response);
+        alert(response.statusText);
+      }
     });
-    // check the response status
-    if (response.ok) {
-      console.log('success');
-      document.location.replace('/dashboard')
-    } else {
-      console.log(response);
-      alert(response.statusText);
-    }
-    // response.ok ? console.log('success') : alert(response.statusText);
   }
 }
 
